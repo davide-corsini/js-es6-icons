ffd98b11d9471872af13b2199977fb2d8d520040
davide <davidecorsinimilano@gmail.com>
2021-01-22 14:42:56 +0100

    working on

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
+//{punto#1 Aggiungere i colori ad ogni categoria: 
+//{punto#1 blue per gli animali, orange per i vegetali e viola per gli utenti.
+//{punto#1 Prendete i colori da un altro array.
+
+
+//.punto#2 voglio far corrispondere la const---> colori in ordine ad:
+//. 1)animali   2)vegetali    3) users
+
 $(document).ready(function(){
+
+    //*Questo é il mio array da inserire nel mio foglio html e su cui fare tutti i relativi cicli
     const icons = [
         {
             name: 'cat',
@@ -121,4 +131,18 @@ $(document).ready(function(){
             family: 'fas',
         }
     ];
+
+    //{punto#1 mi creo una constante con i tre colori da assegnare ad ogni genere
+    const colori = [
+        'blue',
+        'orange',
+        'purple'
+    ];
+
+    //.per rispondere al punto #2 devo ciclare sull array principale
+    icons
+
+
+
+
 });
